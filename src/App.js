604cada75683d8305663e5d04a0ced407edf951b
BFordDev <bf@bford.dev>
2020-12-21 10:12:31 -0700

    added particle bg and set up initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ParticlesBg from 'particles-bg'
+
+function App() {
+  return (
+    <Router>
+    <>
+
+    </>
+    <ParticlesBg type="cobweb" bg={true} />
+    </Router>
+  );
+}
+
+export default App;
